ddde231020897e874a818d8297d84dceac157a19
Orlando Fiol <fiolorlando@gmail.com>
2014-01-16 11:49:31 -0430

    Creada aplicacion para gestion de contribuyentes

---
+++ contribuyentes/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Rubros(models.Model):
+    codigo=models.IntegerField()
+    rubro=models.CharField(max_length=100)
+    alicuota=models.FloatField()
+    ut=models.IntegerField()
+    def __unicode__(self):
+        return u'%d %s (%d UT)'%(self.codigo,self.rubro,self.ut)
+
+class Contribuyente(models.Model):
+    id_contrato=models.IntegerField()
+    num_identificacion=models.CharField(max_length=50)
+    nombre=models.CharField(max_length=200)
+    telf=models.CharField(null=True,blank=True,max_length=100)
+    fax=models.CharField(null=True,blank=True,max_length=100)
+    email=models.CharField(null=True,blank=True,max_length=50)
+    representante=models.CharField(null=True,blank=True,max_length=200)
+    rubro=models.ForeignKey(Rubros)
+    def __unicode__(self):
+        return u'%s %s %s'%(self.num_identificacion,self.nombre,self.representante)
+
+
