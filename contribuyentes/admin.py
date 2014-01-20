939c7d8ce59e990f1740afd043695fea405f07d5
Rodrigo Bravo <rodri.brv@gmail.com>
2014-01-20 17:14:45 -0430

    Agregando Contribuyentes al admin

--- contribuyentes/admin.py
+++ contribuyentes/admin.py
@@ -1,3 +1,16 @@
+# -*- coding:utf-8 -*-
 from django.contrib import admin
+from django.conf import settings
+from contribuyentes.models import *
 
-# Register your models here.
+class ContribuyenteAdmin(admin.ModelAdmin):
+    search_fields = ['id_contrato','num_identificacion','nombre','representante','cedula_rep']
+    list_display = ['id_contrato','num_identificacion','nombre','telf','email','representante','cedula_rep']
+    ordering = ['id_contrato']
+admin.site.register(Contribuyente, ContribuyenteAdmin)
+
+class RubroAdmin(admin.ModelAdmin):
+    search_fields = ['codigo','rubro','ut']
+    list_display = ['codigo','rubro','alicuota','ut']
+    ordering = ['codigo']
+admin.site.register(Rubro, RubroAdmin)
