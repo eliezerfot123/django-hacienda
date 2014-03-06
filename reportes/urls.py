9a49a2072509e923c7478dfed9083a5cf328d26f
Rodrigo Bravo (goidor) <rodri.brv@gmail.com>
2014-03-05 22:19:18 -0430

    Reporte Boletín de liquidaciones Estimadas

--- reportes/urls.py
+++ reportes/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
     url(r'^vauches/$', 'reportes.views.vauche_imprimir'),
     url(r'^liquidacion/(\w+)/$', 'reportes.views.liquidacion_report'),
     url(r'^boletin_def/(\w+)/$', 'reportes.views.boletin_liquid_definitiva'),
+    url(r'^boletin_est/(\w+)/$', 'reportes.views.boletin_liquid_estimada'),
 )
